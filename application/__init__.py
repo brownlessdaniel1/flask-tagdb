b92265f502300d61dd7dd162e1541d9b71390fae
brownlessdaniel1 <DBrownless@qa.com>
2021-07-22 15:15:06 +0000

    db: setup project dir structure

---
+++ application/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQL_URI")
+app.config["SECRET_KEY"] = "my_key"
+app.config["SQL_ALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLALCHEMY (app)
+
+from application import routes
